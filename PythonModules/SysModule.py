56416d202199e0f89245fa1e297d7faaa3c0f816
arjun1321 <guptaarjun152@gmail.com>
2016-09-17 11:46:24 +0530

    sys module

---
+++ PythonModules/SysModule.py
@@ -0,0 +1,10 @@
+import sys
+
+# inputStatement = sys.stdin.readline()
+
+# inputStatement = sys.stdin.readline(10)
+# print(inputStatement)
+
+sys.stdout.write("This statement is awesome!\n")
+
+print(sys.version)
