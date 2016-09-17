3f490ad24dd936b3924f81fe73ff4ca2201709ae
arjun1321 <guptaarjun152@gmail.com>
2016-09-17 13:30:41 +0530

    for loops in turtle

---
+++ PythonModules/TurtleModule.py
@@ -0,0 +1,32 @@
+import turtle
+import time
+
+# Square
+# t = turtle.Pen()
+# t.forward(50)
+# t.left(90)
+# t.forward(50)
+# t.left(90)
+# t.forward(50)
+# t.left(90)
+# t.forward(50)
+
+# Octagon
+# t = turtle.Pen()
+# for i in range(0,8):
+#     t.forward(50)
+#     t.left(45)
+
+# Star
+# t = turtle.Pen()
+# for i in range(1,38):
+#     t.forward(100)
+#     t.left(175)
+
+
+t = turtle.Pen()
+for i in range (1,20):
+    t.forward(100)
+    t.left(95)
+
+time.sleep(3)
